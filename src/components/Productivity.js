e349653d32f5f60b97b1fda40c81a39217434eb3
Prakhar-1912 <rajs191201@gmail.com>
2023-11-20 01:15:29 +0530

    final commit

--- src/components/Productivity.js
+++ src/components/Productivity.js
@@ -34,10 +34,7 @@ const Productivity = () => {
           <h2>Employee Productivity Dashboard</h2>
           <div className="productivity">
             {productivity_data.map((item) => {
-              // Extract the numeric value from the percentage string
             const percentageValue = parseInt(item.percentage, 10) || 0;
-            
-            // Calculate the width of the progress bar dynamically
             const progressBarWidth = `${(percentageValue / 100) * 50}%`;
 
               return (
